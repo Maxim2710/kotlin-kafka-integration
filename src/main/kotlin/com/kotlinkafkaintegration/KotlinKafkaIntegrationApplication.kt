3b52c475440351f3299c92f2bdd9eb4177eb97ad
PrimakMV <pm2710@mail.ru>
2025-02-24 22:25:56 +0300

    Testing Kafka's work

--- src/main/kotlin/com/kotlinkafkaintegration/KotlinKafkaIntegrationApplication.kt
+++ src/main/kotlin/com/kotlinkafkaintegration/KotlinKafkaIntegrationApplication.kt
@@ -1,11 +1,20 @@
 package com.kotlinkafkaintegration
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.kafka.core.KafkaTemplate
 
 @SpringBootApplication
-class KotlinKafkaIntegrationApplication
+class KotlinKafkaIntegrationApplication {
+	@Bean
+	fun commandLineRunner(kafkaTemplate: KafkaTemplate<String, String>) = CommandLineRunner { args ->
+		kafkaTemplate.send("test", "hello kafka")
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<KotlinKafkaIntegrationApplication>(*args)
 }
+
